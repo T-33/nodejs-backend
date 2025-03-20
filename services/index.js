4089a2b0f0f36ee0ea41efaf6ddc1be94012a6a4
Timur Rakhmatullaev <timurrahmatullaev703@gmail.com>
2025-03-20 20:25:19 +0600

    add service layer

--- services/index.js
+++ services/index.js
@@ -1,14 +1,18 @@
-import {getKnex} from "../knex.js";
+import { getKnex } from '../knex.js';
 
 export async function getAllUsers() {
+    const knex = await getKnex();
+    const users = await knex('users')
+        .select('*');
 
+    return users;
 }
 
 export async function getUserById(id) {
-    const knex = await getKnex()
-    const user = await knex("users")
+    const knex = await getKnex();
+    const user = await knex('users')
         .where({ id })
-        .first()
+        .first();
 
     return user;
 }
