d16e286ffeeeab49bfb3fbdf1c7d6c4975271ed7
Timur Rakhmatullaev <timurrahmatullaev703@gmail.com>
2025-03-20 20:25:43 +0600

    add middleware and try-catch

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 import Koa from 'koa';
 import bodyparser from 'koa-bodyparser';
-import { userRouter } from './controllers/index.js';
-import { postRouter } from './controllers/posts.js';
 import { getKnex } from './knex.js';
+import { HTTP_PORT } from './utils/config.js';
+
+import {
+    usersRouter,
+    authRouter,
+    postsRouter,
+} from './controllers/index.js';
 
 async function main() {
     console.log('start', new Date());
@@ -13,28 +18,66 @@ async function main() {
 
     const app = new Koa();
     app.use(bodyparser());
+
+    app.use(async (ctx, next) => {
+        console.log(ctx.method, ctx.url, ctx.body);
+        await next();
+    });
+
+    app.use(authRouter.routes());
+
+    app.use(async (ctx, next) => {
+        const { headers } = ctx.request;
+
+        console.log(headers);
+        const { authorization } = headers;
+
+        const token = authorization?.split(' ')[1];
+
+        console.log({ token });
+
+        if (token) {
+            const userInfo = (await knex.raw(`
+                select * from tokens
+                inner join users
+                    on users.id = tokens.user_id
+                where tokens.token = ?
+            `, [token])).rows[0];
+
+            if (!userInfo) {
+                throw new Error('Unauthorized');
+            }
+
+            ctx.state.user = userInfo;
+        }
+
+        return next();
+    });
+
     app.use(async (ctx, next) => {
         try {
             await next();
-            console.log('after request try-catch');
         } catch (e) {
+            if (e.isJoi) {
+                ctx.status = 400;
+                ctx.body = {
+                    message: e.message,
+                };
+                return;
+            }
             console.log(e);
 
             ctx.status = 500;
-            ctx.body = 'Something went wrong';
+            ctx.body = {
+                message: e.message,
+            };
         }
     });
-    app.use(async (ctx, next) => {
-        console.log(ctx.method, ctx.url, ctx.body);
-        console.log('after request in logger');
-        return next();
-    });
-    app.use(userRouter.routes());
-    app.use(postRouter.routes());
+
+    app.use(usersRouter.routes());
+    app.use(postsRouter.routes());
 
     console.log(res.rows);
-
-    const HTTP_PORT = 8080;
 
     app.listen(HTTP_PORT, () => {
         console.log('server started at ', HTTP_PORT);
